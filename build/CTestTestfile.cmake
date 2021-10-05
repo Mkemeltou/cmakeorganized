908e7a62726c16865bd46b7dbe1527d9d7f94878
M'Kemeltou Oumar <oumar4u@etu.univ-lorraine.fr>
2021-10-05 16:02:15 +0200

    version fonctionne

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: O:/Documents/S9/Cmake/github/cmakeorganized
+# Build directory: O:/Documents/S9/Cmake/github/cmakeorganized/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src/log_lib")
+subdirs("tests")
