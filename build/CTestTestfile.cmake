0f968d2cb42a04dadc1ba9ad19334533aabb533e
M'Kemeltou Oumar <oumar4u@etu.univ-lorraine.fr>
2021-10-05 14:57:01 +0200

    second commit organizedcmake

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: O:/Documents/S9/Cmake/Organized_CMake
-# Build directory: O:/Documents/S9/Cmake/Organized_CMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/log_lib")
-subdirs("mains")
-subdirs("tests")
--- build/install/include/log_lib/print_log.h
+++
@@ -1,6 +0,0 @@
-#pragma once
- 
-//__declspec(dllexport) void __cdecl hello_world();
-//__declspec(dllexport) void __cdecl print_version();
-void  hello_world();
-void  print_version();
